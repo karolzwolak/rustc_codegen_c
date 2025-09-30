f807b1a6eb10c283936754672b2f95eba3a3d4fd
Karol Zwolak <karolzwolak7@gmail.com>
2025-09-30 20:53:04 +0200

    bless tests

--- tests/bless/basic_math.c
+++ tests/bless/basic_math.c
@@ -1,4 +1,4 @@
-// file: basic_math.3cfc46df15d2d47-cgu.0.c
+// file: basic_math.7206e0a37c6be10d-cgu.0.c
 #include <stdint.h>
 
 /* Some helper macros for the generated code */
@@ -14,13 +14,13 @@
 #define __rust_utos(u, s, v, m) \
     ((v) <= (m) ? ((s)v) : ((s)((u)(v) - (u)(m) - 1)))
 
-int32_t main();
 int64_t foo(uint8_t _0, uint16_t _1, uint32_t _2);
-
-int32_t main() { return 0; }
+int32_t main();
 
 int64_t foo(uint8_t _0, uint16_t _1, uint32_t _2)
 {
   int64_t _3 = __rust_utos(uint64_t, int64_t, (int64_t) _0, INT64_MAX);
   return _3;
 }
+
+int32_t main() { return 0; }
