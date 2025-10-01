b47e3af1212500c56d3d79432c03f10cb988d30e
Karol Zwolak <karolzwolak7@gmail.com>
2025-10-01 20:27:55 +0200

    restore fatal errors

--- crates/rustc_codegen_c/src/write.rs
+++ crates/rustc_codegen_c/src/write.rs
@@ -4,7 +4,7 @@ use std::process::{Command, Stdio};
 
 use rustc_codegen_ssa::back::write::{CodegenContext, ModuleConfig};
 use rustc_codegen_ssa::{CompiledModule, ModuleCodegen};
-use rustc_errors::{DiagCtxtHandle, FatalError};
+use rustc_errors::DiagCtxtHandle;
 use rustc_session::config::OutputType;
 use tracing::error;
 
@@ -12,7 +12,7 @@ pub(crate) fn codegen(
     cgcx: &CodegenContext<crate::CCodegen>,
     module: ModuleCodegen<String>,
     _config: &ModuleConfig,
-) -> Result<CompiledModule, FatalError> {
+) -> CompiledModule {
     let dcx = cgcx.create_dcx();
     let dcx = dcx.handle();
     let obj_out = cgcx.output_filenames.temp_path_for_cgu(
@@ -23,9 +23,16 @@ pub(crate) fn codegen(
     let c_out = obj_out.with_extension("c");
 
     // output c source code
-    let c_out_file = fs::File::create(&c_out).map_err(|_| FatalError)?;
-    writeln!(&c_out_file, "// file: {}.c", module.name).map_err(|_| FatalError)?;
-    write!(&c_out_file, "{}", module.module_llvm).map_err(|_| FatalError)?;
+    // dcx.fatal() exits so unwrap is fine
+    let c_out_file = fs::File::create(&c_out)
+        .map_err(|err| dcx.fatal(format!("Failed to create C source file: {err}")))
+        .unwrap();
+    writeln!(&c_out_file, "// file: {}.c", module.name)
+        .map_err(|err| dcx.fatal(format!("Failed to write to C source file: {err}")))
+        .unwrap();
+    write!(&c_out_file, "{}", module.module_llvm)
+        .map_err(|err| dcx.fatal(format!("Failed to write to C source file: {err}")))
+        .unwrap();
 
     // invoke cc to compile
     // FIXME: configure cc
@@ -42,17 +49,17 @@ pub(crate) fn codegen(
         Ok(output) => output,
         Err(e) => {
             error!("failed to spawn C compiler: {}", e);
-            return Err(FatalError);
+            dcx.fatal(format!("failed to spawn C compiler: {e}"));
         }
     };
 
     if !output.status.success() {
         error!("compiler stderr:\n{}", String::from_utf8_lossy(&output.stderr));
         error!("compiler stdout:\n{}", String::from_utf8_lossy(&output.stdout));
-        return Err(FatalError);
+        dcx.fatal("C compiler failed to compile module");
     }
 
-    Ok(module.into_compiled_module(
+    module.into_compiled_module(
         true,
         false,
         false,
@@ -60,13 +67,13 @@ pub(crate) fn codegen(
         false,
         &cgcx.output_filenames,
         cgcx.invocation_temp.as_deref(),
-    ))
+    )
 }
 
 pub(crate) fn link(
     _cgcx: &CodegenContext<crate::CCodegen>,
     _dcx: DiagCtxtHandle<'_>,
     mut _modules: Vec<ModuleCodegen<String>>,
-) -> Result<ModuleCodegen<String>, FatalError> {
+) -> ModuleCodegen<String> {
     unimplemented!();
 }
