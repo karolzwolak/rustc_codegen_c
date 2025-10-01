ce15d755fc8deefb264a713e0c000d331cbc3db6
Karol Zwolak <karolzwolak7@gmail.com>
2025-10-01 20:12:20 +0200

    Revert "implement error handling for codegen()"
    This reverts commit 14d3c55f2abb80bafebe609b408ed71c29bfbea2.

--- crates/rustc_codegen_c/messages.ftl
+++
@@ -1,5 +0,0 @@
-codegen_c_cc_spawn_err = Failed to spawn C compiler : {$err}
-
-codegen_c_cc_err = C compiler returned an error. stderr : `{$cc_stderr}`; stdout : `{$cc_stdout}`
-
-codegen_c_c_file_write_err = Failed to write C source file : {$err}
--- crates/rustc_codegen_c/src/write.rs
+++ crates/rustc_codegen_c/src/write.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::io::{self, Write};
-use std::path::PathBuf;
+use std::io::Write;
 use std::process::{Command, Stdio};
 
 use rustc_codegen_ssa::back::write::{CodegenContext, ModuleConfig};
@@ -9,33 +8,11 @@ use rustc_errors::{DiagCtxtHandle, FatalError};
 use rustc_session::config::OutputType;
 use tracing::error;
 
-fn write_module(c_out: &PathBuf, module: &ModuleCodegen<String>) -> io::Result<()> {
-    let mut c_out_file = fs::File::create(c_out)?;
-    writeln!(c_out_file, "// file: {}.c", module.name)?;
-    write!(c_out_file, "{}", module.module_llvm)?;
-    Ok(())
-}
-fn compiled_module(
-    success: bool,
-    module: ModuleCodegen<String>,
-    cgcx: &CodegenContext<crate::CCodegen>,
-) -> CompiledModule {
-    module.into_compiled_module(
-        success,
-        false,
-        false,
-        false,
-        false,
-        &cgcx.output_filenames,
-        cgcx.invocation_temp.as_deref(),
-    )
-}
-
 pub(crate) fn codegen(
     cgcx: &CodegenContext<crate::CCodegen>,
     module: ModuleCodegen<String>,
     _config: &ModuleConfig,
-) -> CompiledModule {
+) -> Result<CompiledModule, FatalError> {
     let dcx = cgcx.create_dcx();
     let dcx = dcx.handle();
     let obj_out = cgcx.output_filenames.temp_path_for_cgu(
@@ -46,10 +23,9 @@ pub(crate) fn codegen(
     let c_out = obj_out.with_extension("c");
 
     // output c source code
-    if let Err(c_out_err) = write_module(&c_out, &module) {
-        dcx.emit_err(crate::errors::CFileWriteError { err: c_out_err });
-        return compiled_module(false, module, cgcx);
-    }
+    let c_out_file = fs::File::create(&c_out).map_err(|_| FatalError)?;
+    writeln!(&c_out_file, "// file: {}.c", module.name).map_err(|_| FatalError)?;
+    write!(&c_out_file, "{}", module.module_llvm).map_err(|_| FatalError)?;
 
     // invoke cc to compile
     // FIXME: configure cc
@@ -57,30 +33,34 @@ pub(crate) fn codegen(
     // FIXME: flush_linked_file (windows)
     let mut cmd = Command::new("clang");
     cmd.arg(&c_out).arg("-o").arg(&obj_out).arg("-c");
-    let success = match cmd
+    let output = match cmd
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
         .and_then(|child| child.wait_with_output())
     {
-        Ok(output) if !output.status.success() => {
-            error!("compiler stderr:\n{}", String::from_utf8_lossy(&output.stderr));
-            error!("compiler stdout:\n{}", String::from_utf8_lossy(&output.stdout));
-            dcx.emit_err(crate::errors::CCompilerError {
-                cc_stderr: String::from_utf8_lossy(&output.stderr).to_string(),
-                cc_stdout: String::from_utf8_lossy(&output.stdout).to_string(),
-            });
-            false
-        }
-        Ok(_) => true,
+        Ok(output) => output,
         Err(e) => {
             error!("failed to spawn C compiler: {}", e);
-            dcx.emit_err(crate::errors::CCompilerSpawnError { err: e });
-            false
+            return Err(FatalError);
         }
     };
 
-    compiled_module(success, module, cgcx)
+    if !output.status.success() {
+        error!("compiler stderr:\n{}", String::from_utf8_lossy(&output.stderr));
+        error!("compiler stdout:\n{}", String::from_utf8_lossy(&output.stdout));
+        return Err(FatalError);
+    }
+
+    Ok(module.into_compiled_module(
+        true,
+        false,
+        false,
+        false,
+        false,
+        &cgcx.output_filenames,
+        cgcx.invocation_temp.as_deref(),
+    ))
 }
 
 pub(crate) fn link(
